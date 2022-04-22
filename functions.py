31cadc2e53cfc0fae92f1366421db2ea62e2ce37
harsh <melili5339@procowork.com>
2022-04-22 20:32:30 +0530

    test

--- functions.py
+++ functions.py
@@ -1,10 +1,25 @@
-from cgitb import reset
+def multipy(x,y):
+    x_y = x*y
+    return x_y
+
+def is_palindrome(string):
+    return string[::-1].casefold() == string.casefold()
+
+word = input("Please enter the world to check: ")
+if is_palindrome(word):
+    print("'{}' is plaindrome".format(word))
+else:
+    print("'{}' is not palindrome".format(word))
 
 
-def multiply():
-    result = 10.5*4
-    return result
+# answer = multipy(10,45)
+# print(answer)
 
 
-answer = multiply()
-print(answer)
+# a_b = multipy(4,8)
+# print(a_b)
+
+# for pav in range(1,6):
+#     van = multipy(6,pav)
+#     print(van)
+
